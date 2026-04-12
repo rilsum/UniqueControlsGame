6a92e1861b018ab57a68dee781f6d32fe66912ad
rilsum <102576817@student.swin.edu.au>
2026-04-12 17:52:03 +1000

    Updated UI and added animation

--- UniqueControlsGame/Assets/Scripts/KeyInput.cs
+++ UniqueControlsGame/Assets/Scripts/KeyInput.cs
@@ -8,6 +8,8 @@ using UnityEngine.UI;
     public AudioSource KeyAud;
     public Color updateColour = Color.red;
     public Color revertColour = Color.white;
+    public GameObject effectPrefab;
+    public Transform spawnEffect;
 }
 public class KeyInput : MonoBehaviour
 {
@@ -24,12 +26,25 @@ public class KeyInput : MonoBehaviour
     {
         foreach (KeyObject k in keys)
         {
+            //initiating sound and color chane of key
             if (Input.GetKeyDown(k.key))
             {
             Debug.Log(k.key + " Sound!");
             k.KeyImg.color = k.updateColour;
             k.KeyAud.Play();
+                        // adding sprite visual effects when key is pressed
+            if (k.effectPrefab != null && k.spawnEffect != null){
+                GameObject effect = Instantiate (
+                    k.effectPrefab,
+                    k.spawnEffect.position,
+                    Quaternion.identity,
+                    k.spawnEffect
+                );
+                Destroy(effect, 1.5f);
             }
+            }
+
+
             if (Input.GetKeyUp(k.key))
             {
                 k.KeyImg.color = k.revertColour;
